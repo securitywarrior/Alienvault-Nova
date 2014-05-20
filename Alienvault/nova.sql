6aff496108f8b4bafcf7003f18901f57dc9a3c61
Tim Brigham <timb@cfcausa.org>
2014-05-20 11:14:58 -0500

    Update to standardize plugin IDs

--- Alienvault/nova.sql
+++ Alienvault/nova.sql
@@ -1,10 +1,10 @@
-DELETE FROM plugin WHERE id = "9905";
-DELETE FROM plugin_sid where plugin_id = "9905";
+DELETE FROM plugin WHERE id = "10001";
+DELETE FROM plugin_sid where plugin_id = "10001";
 
 # Register the plugin
-INSERT IGNORE INTO plugin (id, type, name, description, vendor, product_type ) VALUES (9905, 1, 'nova', 'Nova Alerts', 'TimB', 11 );
+INSERT IGNORE INTO plugin (id, type, name, description, vendor, product_type ) VALUES (10001, 1, 'nova', 'Nova Alerts', 'TimB', 11 );
 
 # And the individual SID values
-INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, subcategory_id, class_id, name, priority, reliability) VALUES (9905, 1, 19, 225, NULL, 'Nova Alert', 5, 10);
+INSERT IGNORE INTO plugin_sid (plugin_id, sid, category_id, subcategory_id, class_id, name, priority, reliability) VALUES (10001, 1, 19, 225, NULL, 'Nova Alert', 5, 10);
 
 # Remember that once this is complete be sure to run alienvault-reconfig after importing these definitions!
